92f1bb2d36d3fadc22c42be0330d1e3119aa58bb
Arturo <arturo.gil@umh.es>
2024-03-18 11:46:03 +0100

    Refactor

--- extract_rosbag.py
+++ extract_rosbag.py
@@ -17,10 +17,6 @@ robot0/
 @Authors: Antonio Santo and Arturo Gil
           arturo.gil@umh.es
 @Time: November 2022
-
-pip install --ignore-installed  --extra-index-url https://rospypi.github.io/simple/ rospy rosbag sensor-msgs geometry-msgs cv_bridge
-pip install opencv-python
-pip install requirements.txt
 """
 import rosbag
 import yaml
@@ -46,19 +42,11 @@ if __name__ == '__main__':
     rosbag_path = param_list.get('rosbag_path')
     output_path = param_list.get('output_path')
     # extract topics?
-    extract_odometry = param_list.get('extract_topics').get('odometry')
-    extract_lidar2csv = param_list.get('extract_topics').get('lidar2csv')
-    extract_lidar2pcd = param_list.get('extract_topics').get('lidar2pcd')
-    extract_ground_truth = param_list.get('extract_topics').get('ground_truth')
-    extract_gps = param_list.get('extract_topics').get('gps')
-    extract_imu = param_list.get('extract_topics').get('imu')
-    extract_camera = param_list.get('extract_topics').get('camera')
-    extract_tf = param_list.get('extract_topics').get('tf')
-    extract_tf_static = param_list.get('extract_topics').get('tf_static')
-    extract_gps_filtered = param_list.get('extract_topics').get('gps_filtered')
-    extract_odometry_gps = param_list.get('extract_topics').get('odometry_gps')
+    save_point_cloud_as_csv = param_list.get('save_point_cloud_as_csv')
+    save_point_cloud_as_pcd = param_list.get('save_point_cloud_as_pcd')
 
-    print('POINT CLOUD TOPIC: ', topic_name_point_cloud)
+    print('POINT CLOUD TOPIC (LiDAR): ', topic_name_point_cloud)
+    print('GROUND TRUTH TOPIC: ', topic_name_ground_truth)
     print('ODOMETRY TOPIC: ', topic_name_odometry)
     print('GPS TOPIC: ', topic_name_gps)
     print('IMU TOPIC: ', topic_name_imu)
@@ -66,79 +54,91 @@ if __name__ == '__main__':
 
     # WRITE ROBOT0 DIRECTORY
     eurocsaver = EurocSaver(euroc_directory=output_path)
-
+    print("Loading Rosbag file!")
     # READ ROSBAG FILE
     bag = rosbag.Bag(rosbag_path)
-    print("Rosbag loaded")
-    if extract_gps_filtered:
+    print("Rosbag loaded!")
+    # this topic may only be available if using simulation in ROS/Gazebo
+    if topic_name_ground_truth:
         try:
-            print('Saving: ', topic_name_odometry_gps)
-            eurocsaver.save_odometry_gps(bag, topic_name_odometry_gps)
+            print('Saving topic: ', topic_name_ground_truth)
+            eurocsaver.save_ground_truth(bag, topic_name_ground_truth)
         except IndexError:
-            print('Error saving', topic_name_odometry_gps)
+            print('Error saving', topic_name_ground_truth)
+            print('The topic name may be non-existent')
             pass
-    if extract_gps_filtered:
+    if topic_name_odometry:
+        try:
+            print('Saving topic_name_odometry: ', topic_name_odometry)
+            eurocsaver.save_odometry(bag, topic_name_odometry)
+        except IndexError:
+            print('Error saving', topic_name_odometry)
+            print('The topic name may be non-existent')
+            pass
+    if topic_name_gps:
+        try:
+            print('Saving topic_name_gps: ', topic_name_gps)
+            eurocsaver.save_gps(bag, topic_name_gps)
+        except IndexError:
+            print('Error saving', topic_name_gps)
+            print('The topic name may be non-existent')
+            pass
+    if topic_name_gps_filtered:
         try:
             print('Saving: ', topic_name_gps_filtered)
             eurocsaver.save_gps_filtered(bag, topic_name_gps_filtered)
         except IndexError:
             print('Error saving', topic_name_gps_filtered)
+            print('The topic name may be non-existent')
             pass
-    if extract_tf:
+    if topic_name_odometry_gps:
         try:
-            print('Saving: ', topic_name_tf)
-            eurocsaver.save_tf(bag, topic_name_tf)
+            print('Saving: ', topic_name_odometry_gps)
+            eurocsaver.save_odometry_gps(bag, topic_name_odometry_gps)
         except IndexError:
-            print('Error saving', topic_name_tf)
+            print('Error saving', topic_name_odometry_gps)
+            print('The topic name may be non-existent')
             pass
-    if extract_tf_static:
+    if topic_name_imu:
         try:
-            print('Saving: ', topic_name_tf_static)
-            eurocsaver.save_tf_static(bag, topic_name_tf_static)
-        except IndexError:
-            print('Error saving', topic_name_tf_static)
-            pass
-    if extract_odometry:
-        try:
-            print('Saving: ', topic_name_odometry)
-            eurocsaver.save_odometry(bag, topic_name_odometry)
-        except IndexError:
-            print('Error saving', topic_name_odometry)
-            pass
-    if extract_gps:
-        try:
-            print('Saving: ', topic_name_gps)
-            eurocsaver.save_gps(bag, topic_name_gps)
-        except IndexError:
-            print('Error saving', topic_name_gps)
-            pass
-    if extract_imu:
-        try:
-            print('Saving: ', topic_name_imu)
+            print('Saving topic_name_imu: ', topic_name_imu)
             eurocsaver.save_imu(bag, topic_name_imu)
         except IndexError:
             print('Error saving', topic_name_imu)
+            print('The topic name may be non-existent')
             pass
-    if extract_lidar2csv or extract_lidar2pcd:
-        try:
-            print('Saving: ', topic_name_point_cloud)
-            eurocsaver.save_lidar(bag, topic_name_point_cloud, extract_lidar2csv, extract_lidar2pcd)
-        except IndexError:
-            print('Error saving', topic_name_point_cloud)
-            pass
-    if extract_camera:
+    if topic_name_camera:
         try:
             print('Saving: ', topic_name_camera)
             eurocsaver.save_camera(bag, topic_name_camera)
         except IndexError:
             print('Error saving', topic_name_camera)
+            print('The topic name may be non-existent')
             pass
-    if extract_ground_truth:
+    if topic_name_point_cloud:
+        print('Saving LiDAR points as csv: ', save_point_cloud_as_csv)
+        print('Saving LiDAR points as pcd: ', save_point_cloud_as_pcd)
         try:
-            print('Saving: ', topic_name_ground_truth)
-            eurocsaver.save_ground_truth(bag, topic_name_ground_truth) #just for simulation
+            print('Saving: ', topic_name_point_cloud)
+            eurocsaver.save_lidar(bag, topic_name_point_cloud,
+                                  to_csv=save_point_cloud_as_csv,
+                                  to_pcd=save_point_cloud_as_pcd)
         except IndexError:
-            print('Error saving', topic_name_ground_truth)
+            print('Error saving', topic_name_point_cloud)
+            pass
+    if topic_name_tf:
+        try:
+            print('Saving topic_name_tf: ', topic_name_tf)
+            eurocsaver.save_tf(bag, topic_name_tf)
+        except IndexError:
+            print('Error saving', topic_name_tf)
+            pass
+    if topic_name_tf_static:
+        try:
+            print('Saving topic_name_tf_static: ', topic_name_tf_static)
+            eurocsaver.save_tf_static(bag, topic_name_tf_static)
+        except IndexError:
+            print('Error saving', topic_name_tf_static)
             pass
 
     print('FINISHED!')
